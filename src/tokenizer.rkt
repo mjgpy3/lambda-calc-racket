b6fb9f8d9adbc08c46617965ae6dff64a1bc8724
Michael Gilliland <mjg.py3@gmail.com>
2016-05-17 23:19:04 -0400

    First draft at tokenizer. I'd like to use sets a little more.

--- src/tokenizer.rkt
+++ src/tokenizer.rkt
@@ -1,11 +1,30 @@
 #lang racket
 
+(define alphabet (string->list "abcdefghijklmnopqrstuvwxyz"))
+
 (define (tokenize text)
   (if (= 0 (string-length text))
     '()
-    (cond
-      [else (cons 'lparen (tokenize (substring text 1)))])))
+    (let ([first (string-ref text 0)]
+          [rest (substring text 1)])
+      (cond
+	[(eq? #\( first) (cons 'lparen (tokenize rest))]
+	[(eq? #\) first) (cons 'rparen (tokenize rest))]
+	[(eq? #\\ first) (cons 'lambda (tokenize rest))]
+	[(eq? #\. first) (cons 'dot (tokenize rest))]
+	[(member first alphabet) (cons (list 'name first) (tokenize rest))]
+	[else (tokenize rest)]))))
 
 (display (tokenize ""))
 (newline)
 (display (tokenize "("))
+(newline)
+(display (tokenize "()"))
+(newline)
+(display (tokenize "(\\)"))
+(newline)
+(display (tokenize "( \\ .  )"))
+(newline)
+(display (tokenize "( \\ x . x  )"))
+(newline)
+(display (tokenize "( \\ x . xyz  )"))
